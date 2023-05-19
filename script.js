b52a94b659738e99cf1a0c3faa3e8d021fc467eb
talalifer <ahmedtalal@gmail.com>
2023-05-19 16:08:58 -0400

    script function start

---
+++ script.js
@@ -0,0 +1,12 @@
+//function that takes the [canvas] element on which to draw
+function drawOnCanvas(canvas) {
+    // Initializing variables to track drawing state and mouse position
+    let isDrawing = false;
+    let x = 0;
+    let y = 0;
+
+    //Get drawing context from the canvas element
+    const ctx = canvas.getContext("2d");
+
+    //Add event listeners for the mouse down event
+}
