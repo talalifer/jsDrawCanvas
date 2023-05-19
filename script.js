e16371292112508b1bb81b923858597154ed2dff
talalifer <ahmedtalal@gmail.com>
2023-05-19 18:08:49 -0400

    script function start

--- script.js
+++ script.js
@@ -8,5 +8,27 @@ function drawOnCanvas(canvas) {
     //Get drawing context from the canvas element
     const ctx = canvas.getContext("2d");
 
-    //Add event listeners for the mouse down event
+    //Add event listeners for when mouse is clicked in canvas
+    canvas.addEventListener("mousedown",
+                            //Record the current mouse position and set isDraw to True
+                            ev => {
+                                x = ev.offsetX;
+                                y = ev.offsetY;
+                                isDrawing = true;
+                            });
+    
+    //Add an event listener for when mouse is unclicked in the window
+    window.addEventListener("mouseup",
+                            ev => {
+                                //If user was drawing, isDrawing == true
+                                if (isDrawing == true){
+                                    //Draw a line from the previous ouse position to the current mouse position
+                                    drawLine(ctx, x, y, ev.offsetX, ev.offsetY);
+                                    //Reset the mouse position to initial state and isDrawing to false
+                                    x = 0;
+                                    y = 0;
+                                    isDrawing = false;
+                                }
+                            });
+    
 }
